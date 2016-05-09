b78a36db543cf59b8cf774ef657dca00c523b4d7
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 14:10:32 +1000

    purdy

--- js/app.js
+++ js/app.js
@@ -1,23 +1,18 @@
 $(document).ready(function() {
 
   var rows = $('.row1, .row2, .row3, .row4');
-
   rows.css({
     'text-align': 'center',
     'padding': '2'
   });
 
-
   var colums = $('.col1, .col2, .col3, .col4');
-  /*Remember gutters later*/
-
   colums.css({
     'float': 'left',
     'width': '25%',
     'padding': '20'
   });
 
-  //refactor me
   $('.col1,.col2,.col3,.col4').mouseover(function() {
     $(this).css({
       'background-color':'black',
@@ -30,4 +25,5 @@ $(document).ready(function() {
       'transform':'none'
     });
   });
+  //Add mouse over sound click
 });
