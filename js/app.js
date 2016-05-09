b1df1d012aaae6f41077896b608ea4c54d917e65
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 12:52:17 +1000

    Added mouse overs to js
    Still need to refactor

--- js/app.js
+++ js/app.js
@@ -3,7 +3,60 @@ $(document).ready(function() {
 var body = $('body');
 
   body.css({
-    'text-align': 'right'
+    'text-align': 'center'
   });
+
+//refactor me
+$('.col1').mouseover(function() {
+  $(this).css({
+    'background-color':'seagreen',
+    'transform': 'scale(1.2)'
+  });
+});
+$('.col1').mouseout(function() {
+  $(this).css({
+    'background-color':'transparent',
+    'transform':'none'
+  });
+});
+
+$('.col2').mouseover(function() {
+  $(this).css({
+    'background-color':'seagreen',
+    'transform': 'scale(1.2)'
+  });
+});
+$('.col2').mouseout(function() {
+  $(this).css({
+    'background-color':'transparent',
+    'transform':'none'
+  });
+});
+
+$('.col3').mouseover(function() {
+  $(this).css({
+    'background-color':'seagreen',
+    'transform': 'scale(1.2)'
+  });
+});
+$('.col3').mouseout(function() {
+  $(this).css({
+    'background-color':'transparent',
+    'transform':'none'
+  });
+});
+
+$('.col4').mouseover(function() {
+  $(this).css({
+    'background-color':'seagreen',
+    'transform': 'scale(1.2)'
+  });
+});
+$('.col4').mouseout(function() {
+  $(this).css({
+    'background-color':'transparent',
+    'transform':'none'
+  });
+});
 
 });
