1fb7898416f1e4e4524259a768691b20996fc2d1
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 15:24:39 +1000

    Game logic to do

--- js/game.js
+++ js/game.js
@@ -6,4 +6,28 @@ var player2Wins = 0;
 var player1Total = 0;
 var player2Total = 0;
 
-WinnerNumbers = [15, 240, 3840, 61440, 4369, 8738, 17476, 34948];
+winnerNumbers = [15, 240, 3840, 61440, 4369, 8738, 17476, 34948];
+
+
+getWinner = function(score){
+  for(var i = 0; i < winnerNumbers.length; i++){
+    if (winnerNumbers[i] == winnerNumbers[i]){}
+      console.log("Huh!?");
+
+  }
+};
+
+//set div's to empty
+
+//Get player order
+  //Player 1 first
+  //Player 2 second
+
+//if the first player clicks then append an X
+  //Change the state of that div to filled
+  //Assign number to player 1
+
+//if the seconds player clicks on a div then append a O
+  //Change the state of that div to filled
+  //Assign number to player 2
+
