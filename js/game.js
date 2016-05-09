9fcecc7e1aa38bcfc7865705146aa87dba5ceea8
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 14:40:48 +1000

    Winner Numbers

---
+++ js/game.js
@@ -0,0 +1,9 @@
+//How many wins
+var player1Wins = 0;
+var player2Wins = 0;
+
+//Total num of wins
+var player1Total = 0;
+var player2Total = 0;
+
+WinnerNumbers = [15, 240, 3840, 61440, 4369, 8738, 17476, 34948];
