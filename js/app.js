2ad3a4d6bae91780d7327bdce57149e642754ed2
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 16:25:37 +1000

    four matting

--- js/app.js
+++ js/app.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
 
   var rows = $('.row1, .row2, .row3, .row4');
-  rows.css({
+  rows
+  .css({
     'text-align': 'center',
     'padding': '2'
   });
 
   var colums = $('.col1, .col2, .col3, .col4');
-  colums.css({
+  colums
+  .css({
     'float': 'left',
     'width': '25%',
     'padding': '20'
   });
 
   $('.col1,.col2,.col3,.col4').mouseover(function() {
-    $(this).css({
+    $(this)
+    .css({
       'background-color':'black',
       'transform': 'scale(1.05)'
     });
   });
   $('.col1,.col2,.col3,.col4').mouseout(function() {
-    $(this).css({
+    $(this)
+    .css({
       'background-color':'transparent',
       'transform':'none'
     });
   });
   //Add mouse over sound click
+
+  $('.col1').on('click', function(){
+  $(this)
+  //Append text to div later
+  //Fix up overflow
+  //.append('<img src="assets/giphy.gif" alt="">')
+  //.css({'width': '1'});
+  });
 });
+
