af0ef49cc94415a17473067e5e723f576ffe2aaf
Joel Brewster <j.n.brewster@gmail.com>
2016-05-09 12:57:15 +1000

    le refactor - fin

--- js/app.js
+++ js/app.js
@@ -1,62 +1,22 @@
 $(document).ready(function() {
 
-var body = $('body');
+  var body = $('body');
 
   body.css({
     'text-align': 'center'
   });
 
-//refactor me
-$('.col1').mouseover(function() {
-  $(this).css({
-    'background-color':'black',
-    'transform': 'scale(1.05)'
+  //refactor me
+  $('.col1,.col2,.col3,.col4').mouseover(function() {
+    $(this).css({
+      'background-color':'black',
+      'transform': 'scale(1.05)'
+    });
   });
-});
-$('.col1').mouseout(function() {
-  $(this).css({
-    'background-color':'transparent',
-    'transform':'none'
+  $('.col1,.col2,.col3,.col4').mouseout(function() {
+    $(this).css({
+      'background-color':'transparent',
+      'transform':'none'
+    });
   });
-});
-
-$('.col2').mouseover(function() {
-  $(this).css({
-    'background-color':'black',
-    'transform': 'scale(1.05)'
-  });
-});
-$('.col2').mouseout(function() {
-  $(this).css({
-    'background-color':'transparent',
-    'transform':'none'
-  });
-});
-
-$('.col3').mouseover(function() {
-  $(this).css({
-    'background-color':'black',
-    'transform': 'scale(1.05)'
-  });
-});
-$('.col3').mouseout(function() {
-  $(this).css({
-    'background-color':'transparent',
-    'transform':'none'
-  });
-});
-
-$('.col4').mouseover(function() {
-  $(this).css({
-    'background-color':'black',
-    'transform': 'scale(1.05)'
-  });
-});
-$('.col4').mouseout(function() {
-  $(this).css({
-    'background-color':'transparent',
-    'transform':'none'
-  });
-});
-
 });
