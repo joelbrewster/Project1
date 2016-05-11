b0c000d1f4b8e4553176aeae5649bbed0502dc34
Joel Brewster <j.n.brewster@gmail.com>
2016-05-11 11:05:08 +1000

    Cleaning

--- js/app.js
+++ js/app.js
@@ -5,103 +5,124 @@ $(function() {
       document.getElementById('modal').style.display = "none";
     };
   };
-//Player vars
-var moves = 0;
-var playerRed = 'red';
-var playerBlue = 'blue';
-var currentPlayer = null;
-var printWinner;
+  //Player vars
+  var moves = 0;
+  var playerRed = 'red';
+  var playerBlue = 'blue';
+  var currentPlayer = null;
+  var printWinner;
 
-//Board vars
-var EMPTY = '';
-var clickNum = 0;
-var winBlockRed;
-var winBlockBlue;
+  //Board vars
+  var EMPTY = '';
+  var clickNum = 0;
+  var winBlockRed;
+  var winBlockBlue;
 
-var positions = [
-  EMPTY, EMPTY, EMPTY, EMPTY,
-  EMPTY, EMPTY, EMPTY, EMPTY,
-  EMPTY, EMPTY, EMPTY, EMPTY,
-  EMPTY, EMPTY, EMPTY, EMPTY
-];
+  var positions = [
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY
+  ];
 
-var winConditions = [
-  [0, 1, 2, 3],
-  [4, 5, 6, 7],
-  [8, 9, 10, 11],
-  [12, 13, 14, 15],
-  [0, 4, 8, 12],
-  [1, 5, 9, 13],
-  [2, 6, 10, 14],
-  [3, 7, 11, 15],
-  [0, 5, 10, 15],
-  [3, 6, 9, 12],
-];
+  var winConditions = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    [0, 5, 10, 15],
+    [3, 6, 9, 12],
+  ];
 
-//funk
-function setNextTurn(){
-  if (currentPlayer === playerRed) {
-    currentPlayer = playerBlue;
-  }
-  else {
-    currentPlayer = playerRed;
-  }
-  var messages = $('#messages').text(currentPlayer);
-}
-setNextTurn();
-
-function redWins(){
-
-}
-function blueWins(){
-
-}
-
-$(document).on('click', '#board .square', function(i) {
-  var positionNumber = $(i.currentTarget).index();
-  positions[positionNumber]  = currentPlayer;
-
-  var o;
-  var positionIndex;
-
-  if (currentPlayer == playerRed) {
-  for(i = 0; i < winConditions.length; i++){
-     winBlockRed = [];
-    for( o = 0; o < winConditions[i].length; o++){
-      positionIndex = winConditions[i][o];
-      winBlockRed.push(positions[positionIndex]);
-    }
-    //var allTheSame = true;
-    if(winBlockRed[0] === "red" &&
-    winBlockRed[1] === "red" &&
-    winBlockRed[2] === "red" &&
-    winBlockRed[3] === "red") {
-      redWins();
-      console.log("Red!!!!!");
-    }
+  function stopUpdatingBoard(){
+    winBlockRed = [];
+    winBlockBlue = [];
+    //clear the board
   }
 
-  if (currentPlayer == playerBlue) {
-  for(i = 0; i < winConditions.length; i++){
-     winBlockBlue = [];
-    for( o = 0; o < winConditions[i].length; o++){
-      positionIndex = winConditions[i][o];
-      winBlockBlue.push(positions[positionIndex]);
+  function setNextTurn(){
+    if (currentPlayer === playerRed) {
+      currentPlayer = playerBlue;
     }
-    //var allTheSame = true;
-    if(winBlockBlue[0] === "blue" &&
-    winBlockBlue[1] === "blue" &&
-    winBlockBlue[2] === "blue" &&
-    winBlockBlue[3] === "blue") {
-      blueWins();
-      console.log("Blue!!!!!");
+    else {
+      currentPlayer = playerRed;
+    }
+    var messages = $('#messages').text(currentPlayer);
+  }
+  setNextTurn();
+
+  function redWins(){
+    console.log("red wins!");
+    //change the message at the bottom of the page in the <h1>.
+    //It is "messages" turn. => "messages" Wins!
+    //Load button under the "messages".
+    //Click button to refresh page.
+    //Change refresh page to clear board and start again.
+
+    //TODO
+    //load modal popup
+
+    //refresh page on button click
+  }
+
+  function blueWins(){
+    console.log("blue wins!");
+
+  }
+
+  $(document).on('click', '#board .square', function(i) {
+    var positionNumber = $(i.currentTarget).index();
+    positions[positionNumber]  = currentPlayer;
+
+    var o;
+    var positionIndex;
+
+    if (currentPlayer == playerRed) {
+      for(i = 0; i < winConditions.length; i++){
+        winBlockRed = [];
+        for( o = 0; o < winConditions[i].length; o++){
+          positionIndex = winConditions[i][o];
+          winBlockRed.push(positions[positionIndex]);
+        }
+        var allTheSame = true;
+        if(winBlockRed[0] === "red" &&
+        winBlockRed[1] === "red" &&
+        winBlockRed[2] === "red" &&
+        winBlockRed[3] === "red") {
+
+          //kill the onClick stuff
+          positionIndex = null;
+          positions[positionNumber] = null;
+          redWins();
+          stopUpdatingBoard();
+        }
+      }
+
+      if (currentPlayer == playerBlue) {
+        for(i = 0; i < winConditions.length; i++){
+          winBlockBlue = [];
+          for( o = 0; o < winConditions[i].length; o++){
+            positionIndex = winConditions[i][o];
+            winBlockBlue.push(positions[positionIndex]);
+          }
+          var allTheSame = true;
+          if(winBlockBlue[0] === "blue" &&
+          winBlockBlue[1] === "blue" &&
+          winBlockBlue[2] === "blue" &&
+          winBlockBlue[3] === "blue") {
+            blueWins();
+            stopUpdatingBoard();
+          }
+        }
       }
     }
-  }
-}
 
-$('#board .square:eq(' + positionNumber + ')').addClass(currentPlayer);
+    $('#board .square:eq(' + positionNumber + ')').addClass(currentPlayer);
 
-setNextTurn();
-});
+    setNextTurn();
+  });
 });
