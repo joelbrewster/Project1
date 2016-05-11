b42f50cc56e270dc0cbdf6853dea3609b7a34162
Joel Brewster <j.n.brewster@gmail.com>
2016-05-11 21:33:10 +1000

    Added tie and some logic for next time

--- js/app.js
+++ js/app.js
@@ -2,18 +2,17 @@ $(function() {
   //Load the modal rules window on page load
   window.onload = function () {
     document.getElementById('button').onclick = function () {
-      document.getElementById('modal').style.display = "none";
+      document.getElementById('rule-modal').style.display = "none";
     };
   };
-  //Player vars
+  //Player
   var moves = 0;
   var playerRed = 'red';
   var playerBlue = 'blue';
   var currentPlayer = playerRed;
-  var printWinner;
-  var winner;
+  var winner = null;
 
-  //Board vars
+  //Board
   var EMPTY = '';
   var clickNum = 0;
   var winBlockRed;
@@ -26,6 +25,49 @@ $(function() {
     EMPTY, EMPTY, EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY
   ];
+
+/*
+  var rowLength = 4;
+  var redCount = 0;
+  var blueCount = 0;
+
+  //check horizonal
+  for(var i=0, n=positions.length; i<n; i++){
+    if((i+1)%rowLength == 0){
+      blueCount = 0;
+      redCount = 0;
+    }
+    if(positions[i] == "red"){
+      redCount++;
+    }
+    if(positions[i] == "blue"){
+      blueCount++;
+    }
+    if(blueCount == rowLength){
+      // blue wins
+    }
+    if(redCount == rowLength){
+      // red wins
+    }
+  }
+
+  //check vertical
+  for(var i=0, n=rowLength; i<n; i++){
+    var pos_1 = positions[i*rowLength];
+    var pos_2 = positions[i*rowLength+1];
+    var pos_3 = positions[i*rowLength+2];
+    var pos_4 = positions[i*rowLength+3];
+
+    if(pos_1 == pos_2 && pos_2 == pos_3 && pos_3 == pos_4){
+      if(pos_1 == "red"){
+        //red wins
+      }else if (pos_1 == "blue"){
+        //blue wins
+      }
+
+  }
+
+*/
 
   //Store all the winning combinations a player can use
   var winConditions = [
@@ -48,6 +90,17 @@ $(function() {
     $(document).off('click', '#board .square');
   }
 
+  function checkForTie(){
+    var isTie = true;
+    for (var i = 0; i < positions.length; i++){
+      if(positions[i] == EMPTY){
+        isTie = false;
+        break;
+      }
+    }
+    return isTie;
+
+  }
   function setNextTurn(){
     if (currentPlayer === playerRed) {
       currentPlayer = playerBlue;
@@ -69,7 +122,11 @@ $(function() {
     winBlockRed = [];
     $('#messages').text("Red wins!").css('color','#FA5048');
     stopUpdatingBoard();
+    //Load a modal pop up
+    //Load a button to reset page
     winner = 'Red';
+    //Button
+    //Reset shit
   }
 
   function blueWins(){
@@ -77,11 +134,36 @@ $(function() {
     $('#messages').text("Blue wins!").css('color','#1B75B7');
     stopUpdatingBoard();
     winner = 'Blue';
+    //Button
+    //Reset shit
+  }
+
+  function reset(){
+    $(document).on('click', '#board .square').removeClass('clicked');
+  currentPlayer = playerRed;
+  winner = null;
+
+  //Board
+  EMPTY = '';
+  //var clickNum = 0;
+
+  //Create an empty 'board' where players can click
+  positions = [
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY,
+    EMPTY, EMPTY, EMPTY, EMPTY
+  ];
+
+
   }
 
   //Change function type
   //Simplify - then can call and cancel it on win condition
   $(document).on('click', '#board .square', function(i) {
+    var thisSquare = $(this);
+    if(!thisSquare.hasClass('clicked')){
+      thisSquare.addClass('clicked');
     var positionNumber = $(i.currentTarget).index();
     positions[positionNumber]  = currentPlayer;
 
@@ -134,9 +216,14 @@ $(function() {
           }
         }
       }
-
     //update the positions and current player
     $('#board .square:eq(' + positionNumber + ')').addClass(currentPlayer);
-    setNextTurn();
+    //Check for tie
+    if (checkForTie()){
+      $('#messages').text("It's a tie!");
+      } else {
+      setNextTurn();
+      }
+    }
   });
 });
